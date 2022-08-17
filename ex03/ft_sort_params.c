110da954bc457b9d6a3d5923a0e315476f12e5be
Alfredo A. Hernandez I <alfredo.hernandez@ug.uchile.cl>
2022-08-17 14:39:52 +0200

    wip ex06

--- ex03/ft_sort_params.c
+++ ex03/ft_sort_params.c
@@ -6,10 +6,11 @@
 /*   By: aantonio <aantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 11:31:58 by aantonio          #+#    #+#             */
-/*   Updated: 2022/08/17 14:03:33 by aantonio         ###   ########.fr       */
+/*   Updated: 2022/08/17 14:37:36 by aantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <unistd.h>
 
 void	swap_contents(char *str_array[], int index1, int index2)
@@ -26,15 +27,21 @@ int	check_if_ordered(char *str_array[], int array_length)
 	int	i;
 	int	j;
 
+	printf("begin checking\n");
 	i = 0;
 	while (i < array_length - 1)
 	{
+		printf("	i is %d now, %d < %d\n", i, i, array_length - 1);
 		j = 0;
 		while (str_array[i][j] == str_array[i + 1][j])
 		{
-			if (str_array[i + 1][j] + str_array[i + 1][j] == 0)
+			printf("	\tj is %d now, string: %s with %s\n", j, str_array[i], str_array[i + 1]);
+			if (str_array[i][j] == 0 && str_array[i + 1][j] != 0)
 				break ;
+			if (str_array[i][j] != 0 && str_array[i + 1][j] == 0)
+				return (0);
 			j++;
+			printf("	\t\tj++\n");
 		}
 		if (str_array[i][j] > str_array[i + 1][j])
 			return (0);
@@ -47,23 +54,34 @@ void	order_array(char *str_array[], int array_length)
 {
 	int	i;
 	int	j;
-
+	// printf("array length: %d\n", array_length);
 	i = 0;
 	while (!check_if_ordered(str_array, array_length))
 	{
+		printf("begin order array\n");
+		printf("	i is %d now, %d < %d\n", i, i, array_length - 1);
 		j = 0;
 		while (str_array[i][j] == str_array[i + 1][j])
 		{
-			if (str_array[i + 1][j] + str_array[i + 1][j] == 0)
+			printf("	\tj is %d now, string: %s with %s\n", j, str_array[i], str_array[i + 1]);
+			if (str_array[i][j] == 0 || str_array[i + 1][j] == 0)
 				break ;
 			j++;
 		}
+
 		i = 0;
-		while (i < array_length - 1 && str_array[i][j] <= str_array[i + 1][j])
+		printf("i in order array\n");
+		while (i < (array_length - 1) && str_array[i][j] <= str_array[i + 1][j])
 		{
 			i++;
+			// printf("i is %d now, %d < %d\n", i, i, array_length - 1);
 		}
-		swap_contents(str_array, i, i + 1);
+		// printf("about to swap %s and %s\n", str_array[i], str_array[i + 1]);
+		// printf("i is %d now, %d < %d\n", i, i, array_length - 1);
+		if (i < array_length - 1)
+			swap_contents(str_array, i, i + 1);
+		printf("result: %s %s %s %s %s\n", str_array[0], str_array[1], str_array[2], str_array[3], str_array[4]);
+		// printf("is ordered? %d\n", check_if_ordered(str_array, array_length));
 	}
 }
 
